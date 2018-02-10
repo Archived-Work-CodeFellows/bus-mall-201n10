9f681e50c45b7a94bde7defb84d107b9a3406edc
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-02-10 14:28:23 -0800

    Finished dom manipulation

--- js/app.js
+++ js/app.js
@@ -12,26 +12,25 @@ function ImageGetter(name, path){
   this.path = path;
   this.clicked = 0;
   ImageGetter.all.push(this);
-
 }
 
-new ImageGetter('bag', 'img/bag.jpg');
-new ImageGetter('banana', 'img/banana.jpg');
-new ImageGetter('bathroom', 'img/bathroom.jpg');
-new ImageGetter('boots', 'img/boots.jpg');
-new ImageGetter('breakfast', 'img/breakfast.jpg');
-new ImageGetter('bubblegum', 'img/bubblegum.jpg');
-new ImageGetter('chair', 'img/chair.jpg');
-new ImageGetter('cthulhu', 'img/cthulhu.jpg');
-new ImageGetter('dog-duck', 'img/dog-duck.jpg');
-new ImageGetter('dragon', 'img/dragon.jpg');
-new ImageGetter('pen', 'img/pen.jpg');
-new ImageGetter('pet-sweep', 'img/pet-sweep.jpg');
-new ImageGetter('tauntaun', 'img/tauntaun.jpg');
-new ImageGetter('unicorn', 'img/unicorn.jpg');
-new ImageGetter('usb', 'img/usb.gif');
-new ImageGetter('water-can', 'img/water-can.jpg');
-new ImageGetter('wine-glass', 'img/wine-glass.jpg');
+new ImageGetter('Bag', 'img/bag.jpg');
+new ImageGetter('Banana', 'img/banana.jpg');
+new ImageGetter('Bathroom', 'img/bathroom.jpg');
+new ImageGetter('Boots', 'img/boots.jpg');
+new ImageGetter('Breakfast', 'img/breakfast.jpg');
+new ImageGetter('Bubblegum', 'img/bubblegum.jpg');
+new ImageGetter('Chair', 'img/chair.jpg');
+new ImageGetter('Cthulhu', 'img/cthulhu.jpg');
+new ImageGetter('Dog-duck', 'img/dog-duck.jpg');
+new ImageGetter('Dragon', 'img/dragon.jpg');
+new ImageGetter('Pen', 'img/pen.jpg');
+new ImageGetter('Pet-sweep', 'img/pet-sweep.jpg');
+new ImageGetter('Tauntaun', 'img/tauntaun.jpg');
+new ImageGetter('Unicorn', 'img/unicorn.jpg');
+new ImageGetter('Usb', 'img/usb.gif');
+new ImageGetter('Water-can', 'img/water-can.jpg');
+new ImageGetter('Wine-glass', 'img/wine-glass.jpg');
 
 for(var i = 0; i < 3; i++) images.push(document.getElementById('img'+(i+1)));
 
@@ -47,7 +46,6 @@ function imageDisplay() {
       images[i].src = ImageGetter.all[indexRand].path;
       for(var j = 0; j < images.length; j++){
         if(compare2[j] === images[i].src) {
-          console.log('Previous Iteration at Position ' + i);
           if(i === 0) {
             indexRand = Math.floor(Math.random()*ImageGetter.all.length);
             images[0].src = ImageGetter.all[indexRand].path;
@@ -55,7 +53,6 @@ function imageDisplay() {
           break;
         }
         if(compare[j] === images[i].src) {
-          console.log('Duplicate at position ' + i);
           if(i === 0) {
             indexRand = Math.floor(Math.random()*ImageGetter.all.length);
             images[0].src = ImageGetter.all[indexRand].path;
@@ -81,14 +78,16 @@ function imageDisplay() {
   }
   if(totalClicks === 25) {
     var resultElement = document.getElementById('results');
+    var resultTitle = document.createElement('h1');
+    resultTitle.textContent = 'Results';
     var ulElement = document.createElement('ul');
+    resultElement.appendChild(resultTitle);
     resultElement.appendChild(ulElement);
     for(i = 0; i < ImageGetter.all.length; i++) {
-      console.log(ImageGetter.all[i].name + ' ' + ImageGetter.all[i].clicked);
       var liElement = document.createElement('li');
-      
+      liElement.textContent = ImageGetter.all[i].name + ' voted: ' + ImageGetter.all[i].clicked;
+      ulElement.appendChild(liElement);
     }
   }
-  console.log(totalClicks);
 }
 imageDisplay();
