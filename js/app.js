6edf95dd078a448c41a6768d2b3e56cb1cc93af2
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-02-10 12:24:40 -0800

    Added functionality to have no duplicates appear every 6 imgs

--- js/app.js
+++ js/app.js
@@ -40,13 +40,29 @@ function imageDisplay() {
   totalClicks++;
   var indexRand = 0;
   for(var k = 0; k < images.length; k++) compare2[k] = images[k].src;
-
   for(var i = 0; i < images.length; i++) {
     indexRand = Math.floor(Math.random()*ImageGetter.all.length);
     images[i].src = ImageGetter.all[indexRand].path;
+    for(var j = 0; j < images.length; j++){
+      if(compare2[j] === images[i].src) {
+        console.log('Previous Iteration at ' + i);
+        if(i === 0) {
+          indexRand = Math.floor(Math.random()*ImageGetter.all.length);
+          images[0].src = ImageGetter.all[indexRand].path;
+        } else i--;
+        break;
+      }
+      if(compare[j] === images[i].src) {
+        console.log('Duplicate at position' + i);
+        if(i === 0) {
+          indexRand = Math.floor(Math.random()*ImageGetter.all.length);
+          images[0].src = ImageGetter.all[indexRand].path;
+        } else i--;
+        break;
+      }
+    }
+    compare[i] = images[i].src;
   }
-  // console.log(i);
-  compare[i] = images[i].src;
-
+  console.log(totalClicks);
 }
 imageDisplay();
